21ab8fffebf4c1e786078e65d0e69e23cd9ecb7f
Faysal AlZoubi <faysal.zoabi@gmail.com>
2019-01-21 17:53:25 +0100

    second commit

--- src/tourData.js
+++ src/tourData.js
@@ -25,6 +25,39 @@ export const tourData = [
   },
   {
     id: 4,
+    city: "rome",
+    img: "./img/tokyo.jpeg",
+    name: "tokyo sushi tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+
+  {
+    id: 5,
+    city: "new jersy",
+    img: "./img/newyork.jpeg",
+    name: "new york bridge tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 6,
+    city: "genve",
+    img: "./img/paris.jpeg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 7,
+    city: "berlin",
+    img: "./img/london.jpeg",
+    name: "london royal palace tour",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 8,
     city: "tokyo",
     img: "./img/tokyo.jpeg",
     name: "tokyo sushi tour",
